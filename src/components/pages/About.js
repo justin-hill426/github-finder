7fec32d2cb140b3420ae477e3c9c900b0e4c023c
justin-hill426 <jrhill426@gmail.com>
2022-03-01 17:13:16 -0700

    Updated the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,27 +1,18 @@
 function About() {
   return (
     <>
-      <h1 className='text-6xl mb-4 text-center text-neutral-content'>Github Finder</h1>
+      <h1 className='text-6xl mb-4 text-center text-secondary'>Github Finder</h1>
       <p className='mb-4 text-2xl text-center text-neutral-content'>
         A React app to search GitHub profiles and see profile details. This
-        project is part of the
-        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
-          {' '}
-          React Front To Back
-        </a>{' '}
-        Udemy course by
-        <strong>
-          <a href='https://traversymedia.com'> Brad Traversy</a>
-        </strong>
-        .
+        project uses Daisy UI, Tailwind, and React.
       </p>
       <p className='text-lg text-center text-neutral-content'>
-        Version <span className='text-white'>1.0.0</span>
+        Version <span className='text-white'>1.0.1</span>
       </p>
       <p className='text-lg mb-40 text-center text-neutral-content'>
-        Layout By: {' '}
+        Created By: {' '}
         <a className='text-white' href='https://twitter.com/hassibmoddasser'>
-          Hassib Moddasser
+          Justin Hill
         </a>
       </p>
     </>
